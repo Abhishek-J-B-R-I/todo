b53dd31a2d12bad2d3ce21b7520f5f9657f6958f
bigsur <abhishekmrjbr@outlook.com>
2024-03-20 01:41:20 +0530

    checkbox completed

--- lib/Screens/todotile.dart
+++ lib/Screens/todotile.dart
@@ -1,41 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:todo/Screens/todolist.dart';
 
-class todotile extends StatelessWidget {
+class todotile extends StatefulWidget {
   const todotile({
     super.key,
   });
 
   @override
+  State<todotile> createState() => _todotileState();
+}
+
+class _todotileState extends State<todotile> {
+  bool isCheckedd = true;
+/*
+  void functionforcut(bool? checkstate) {
+    setState(() {
+      isCheckedd = checkstate!;
+    });
+  }*/
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("create TODO app"),
-      trailing: ischeckbox(),
+      title: Text(
+        "create TODO app",
+        style: TextStyle(
+            decoration: isCheckedd ? TextDecoration.lineThrough : null),
+      ),
+      trailing: ischeckbox(
+        ischecked: isCheckedd,
+        funn: (bool? checkstate) {
+          setState(() {
+            isCheckedd = checkstate!;
+          });
+        },
+      ),
     );
   }
 }
 
-class ischeckbox extends StatefulWidget {
-
-
-  @override
-  State<ischeckbox> createState() => _ischeckboxState();
-}
-
-class _ischeckboxState extends State<ischeckbox> {
-  bool ischecked=false;
+class ischeckbox extends StatelessWidget {
+  final bool ischecked;
+  final void Function(bool?) funn;
+  ischeckbox({required this.ischecked, required this.funn});
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       value: ischecked,
-      onChanged: (newValue) {
-        setState(() {
-          if(newValue != null){
-            ischecked=newValue;
-          }
-        });
-
-      },
+      onChanged: funn,
     );
   }
 }
